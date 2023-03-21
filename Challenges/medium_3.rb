a2dcbe3acc19a9d7a87d16fada76cd599d055bee
Lawton Stephens <stephens.lawton@gmail.com>
2023-03-21 09:40:14 -0400

    walkthrough LS solution

--- Challenges/medium_3.rb
+++ Challenges/medium_3.rb
@@ -81,50 +81,123 @@ A:
 
 =end 
 
+# class Clock 
+#   attr_reader :current_time
+
+#   def initialize(hours, minutes=0)
+#     @current_hours = hours 
+#     @current_minutes = minutes 
+#     @current_time = format_time(hours, minutes)
+#   end 
+
+#   def self.at(hours, minutes=0)
+#     Clock.new(hours, minutes)
+#   end 
+
+#   def format_time(hours, minutes=0)
+#     minutes = minutes.digits.size < 2 ? "0#{minutes}" : "%02d" % minutes.to_s
+#     hours = hours.digits.size < 2 ? "0#{hours}" : "%02d" % hours.to_s 
+
+#     "#{hours}:#{minutes}"
+#   end 
+
+#   def +(minutes)
+#     hours_to_add = 0
+#     total_mins = @current_minutes + minutes 
+
+#     while total_mins >= 60 
+#       total_mins -= 60 
+#       hours_to_add += 1
+#     end 
+
+#     @current_hours += hours_to_add
+#     @current_minutes = total_mins 
+
+#     Clock.at(@current_hours, @current_minutes)
+#   end 
+
+#   def to_s 
+#     @current_time 
+#   end
+
+#   def ==(other)
+#     current_time == other.current_time
+#   end 
+
+# end 
+
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+
+# With LS solution walkthrough 
+
 class Clock 
-  attr_reader :current_time
+  attr_reader :hour, :minute 
 
-  def initialize(hours, minutes=0)
-    @current_hours = hours 
-    @current_minutes = minutes 
-    @current_time = format_time(hours, minutes)
+  ONE_DAY = 24 * 60                           # 1440 minutes in one day 
+
+  def initialize(hour, minute)
+    @hour = hour 
+    @minute = minute 
   end 
 
-  def self.at(hours, minutes=0)
-    Clock.new(hours, minutes)
+  def self.at(hour, minute=0)
+    new(hour, minute)
   end 
 
-  def format_time(hours, minutes=0)
-    minutes = minutes.digits.size < 2 ? "0#{minutes}" : "%02d" % minutes.to_s
-    hours = hours.digits.size < 2 ? "0#{hours}" : "%02d" % hours.to_s 
-
-    "#{hours}:#{minutes}"
+  def +(add_minutes)
+    minutes_since_midnight = compute_minutes_since_midnight + add_minutes 
+    while minutes_since_midnight >= ONE_DAY 
+      minutes_since_midnight -= ONE_DAY 
+    end 
+    
+    compute_time_from(minutes_since_midnight)
   end 
 
-  def +(minutes)
-    hours_to_add = 0
-    total_mins = @current_minutes + minutes 
-
-    while total_mins >= 60 
-      total_mins -= 60 
-      hours_to_add += 1
+  def -(sub_minutes)
+    minutes_since_midnight = compute_minutes_since_midnight - sub_minutes
+    while minutes_since_midnight < 0 
+      minutes_since_midnight += ONE_DAY 
     end 
 
-    @current_hours += hours_to_add
-    @current_minutes = total_mins 
-
-    Clock.at(@current_hours, @current_minutes)
+    compute_time_from(minutes_since_midnight)
   end 
 
-  def to_s 
-    @current_time 
+  def ==(other_time)
+    hour == other_time.hour && minute == other_time.minute
   end
 
-  def ==(other)
-    current_time == other.current_time
+  def to_s
+    format('%02d:%02d', hour, minute);
+  end
+
+  private 
+
+  def compute_minutes_since_midnight 
+    total_minutes = 60 * hour + minute 
+    total_minutes % ONE_DAY 
   end 
 
+  def compute_time_from(minutes_since_midnight)
+    hours, minutes = minutes_since_midnight.divmod(60)
+    hours %= 24 
+    self.class.new(hours, minutes)
+  end 
 end 
+
+
+
+
+
+
 
 require 'minitest/autorun'
 
@@ -151,13 +224,11 @@ class ClockTest < Minitest::Test
   end
 
   def test_subtract_fifty_minutes
-    skip 
     clock = Clock.at(0) - 50
     assert_equal '23:10', clock.to_s
   end
 
   def test_subtracting_does_not_mutate
-    skip
     old_clock = Clock.at(10)
     new_clock = old_clock - 50
     refute_same new_clock, old_clock
@@ -169,37 +240,31 @@ class ClockTest < Minitest::Test
   end
 
   def test_wrap_around_at_midnight
-    skip
     clock = Clock.at(23, 30) + 60
     assert_equal '00:30', clock.to_s
   end
 
   def test_add_more_than_a_day
-    skip
     clock = Clock.at(10) + 3061
     assert_equal '13:01', clock.to_s
   end
 
   def test_subtract_a_few_minutes
-    skip
     clock = Clock.at(10, 30) - 5
     assert_equal '10:25', clock.to_s
   end
 
   def test_subtract_minutes
-    skip
     clock = Clock.at(10) - 90
     assert_equal '08:30', clock.to_s
   end
 
   def test_wrap_around_at_negative_midnight
-    skip
     clock = Clock.at(0, 30) - 60
     assert_equal '23:30', clock.to_s
   end
 
   def test_subtract_more_than_a_day
-    skip
     clock = Clock.at(10) - 3061
     assert_equal '06:59', clock.to_s
   end
@@ -219,7 +284,6 @@ class ClockTest < Minitest::Test
   end
 
   def test_wrap_around_backwards
-    skip
     clock1 = Clock.at(0, 30) - 60
     clock2 = Clock.at(23, 30)
     assert_equal clock1, clock2
