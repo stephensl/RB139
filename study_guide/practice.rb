391c7f2646da3e3483c2bd58cadd7c5bec00da8f
Lawton Stephens <stephens.lawton@gmail.com>
2023-04-17 14:23:11 -0400

    more practice

--- study_guide/practice.rb
+++ study_guide/practice.rb
@@ -1,88 +1,46 @@
-=begin
-In this kata, you have an input string and you should check whether it is a valid message. To decide that, you need to split the string by the numbers, and then compare the numbers with the number of characters in the following substring.
+# 1. A closure is a general programming concept that describes saving a "chunk" of code and passing it around to be executed throughout the program. Closures are useful in that they act like anonymous methods, and can be passed around as arguments to other methods. Clsoures maintain their "memory" of the context/environment within which they were created, and can access code artifacts such as local variables, method references, constants, etc. that were in scope at the time the closure was defined. In Ruby, closures are implemented through blocks, `Proc` objects, and `lambda`s. Closures retain access to the variables, constants, and methods that were in scope at the time of their creation, even if those variables, constants, or methods are no longer in scope when the closure is executed.
 
-For example "3hey5hello2hi" should be split into 3, hey, 5, hello, 2, hi and the function should return true, because "hey" is 3 characters, "hello" is 5, and "hi" is 2; as the numbers and the character counts match, the result is true.
+# 2. Variable scope in Ruby is determined where within the program the variable is initialized, and the code constructs surrounding the variable. Typically, local variable scoping rules allow for variables initialized in the outer scope to be available within the inner scope of blocks, but local variables initialized within a block are inacessible outside of the block. Closures are at the heart of variable scoping rules in Ruby and allow us to access variables in areas of the program that would otherwise render them out of scope. A closure's binding allows us to access and manipulate variables that were in scope when the closure was created, and allows for increased flexibility throughout the program. 
 
-Notes:
+# 3. Blocks in Ruby are defined by either curly braces {} or do..end statement. Any Ruby method can take an implicit block, and the way in which the block is utilized is determined by the implementation details of the method. 
 
-Messages are composed of only letters and digits
-Numbers may have multiple digits: e.g. "4code13hellocodewars" is a valid message
-Every number must match the number of character in the following substring, otherwise the message is invalid: e.g. "hello5" and "2hi2" are invalid
-If the message is an empty string, you should return true
+def method_with_block 
+  yield 
+end 
 
-p (valid?("3hey5hello2hi") == true
-p (valid?("4code13hellocodewars") == true
-p (valid?("3hey5hello2hi5") == false
-p (valid?("code4hello5") == false
-p (valid?("1a2bb3ccc4dddd5eeeee") == true
-p (valid?("") == true
+method_with_block { puts "Hello from the block!" }
 
-P: 
+# In the example above, the method `method_with_block` takes an implicit block, and the block is executed by calling the `yield` keyword. The `yield` keyword is a Ruby keyword that executes the block that was passed to the method.
 
-This problem asks for us to split a string by numbers and compare the numbers with the number of characters in the following substring.
+# 4. A `Proc` object is a Ruby object that is used to store a block of code. A `Proc` object can be passed around as an argument to other methods, and can be executed by calling the `call` method on the `Proc` object.`Proc` objects differ from blocks in that they are objects, and can be stored in variables, passed to other methods, and returned from methods. 
 
-Example: 
+proc_example = Proc.new { |x, y| puts x + y }
 
-"3hey5hello2hi" would split 
+result = proc_example.call(1, 2)
+puts result 
 
-: after the last number in a substring of numbers 0-9, and 
-: after the last letter in a substring before either 
-  : a space, 
-  : number, or 
-  : the end of the string.. 
+# 3 
 
-  [3, hey, 5, hello, 2, hi]
+# We can pass the `Proc` into a method by passing it as an argument.
 
-  
-Requirements:
-
-Return true if: 
-
-  : if the string is empty
-
-
-  : string must start with a number &&
-  : string must end with a letter &&
-  : string must have equal number of letters and numbers.
-
-  : Return false if: 
-
-    : string starts with a letter
-    : string ends with a number
-    : string has unequal number of letters and numbers
-    : string has a number that is not followed by a letter
-
-Regex should: 
-
-: may match a number of any length,
-: split after the last number in substring.
-: 1 or more letters followed by a number or end of string
-: repeat pattern 
-: end of string must be a-z character
-
-=end 
-
-
-def valid?(string)
-  return true if string.empty?
-  return false if string.chars.last.match(/[0-9]+/)
-
-  nums = string.scan(/\d+/)
-  letters = string.scan(/[a-z]+/)
-
-  zipped = nums.zip(letters).each do |num, letter|
-    return false if num.nil? || letter.nil? || num.to_i != letter.size
-  end
-
-  true
+def method_with_proc(proc)
+  proc.call 
 end
 
+method_with_proc(proc_example) # => 3
+
+# We can also pass it to the method as a block by using the `&` operator to convert the `Proc` object into a block.
+
+def method_with_proc(&proc)
+  proc.call 
+end 
+
+method_with_proc(&proc_example) # => 3
+
+# We include the `&` operator when passing a `Proc` object as a block to a method because the `&` operator converts the `Proc` object into a block. 
 
 
+# 5. A `lambda` is a Ruby object that is used to store a block of code. A `lambda` is similar to a `Proc` object in that it can be passed around as an argument to other methods, and can be executed by calling the `call` method on the `lambda`. `lambda`s differ from `Proc` objects in that they are more strict about the number of arguments that they accept. `lambda`s also return from the calling method when they are executed, whereas `Proc` objects do not.
 
-p valid?("3hey5hello2hi") == true
-p valid?("4code13hellocodewars") == true
-p valid?("3hey5hello2hi5") == false
-p valid?("code4hello5") == false
-p valid?("1a2bb3ccc4dddd5eeeee") == true
-p valid?("") == true
+# 6. 
+
