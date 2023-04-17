3cc4ad5503d863b00ba29e38fdedfb35c0c3d717
Lawton Stephens <stephens.lawton@gmail.com>
2023-04-17 10:46:44 -0400

    add new challenge questions

---
+++ Challenges/additional.rb
@@ -0,0 +1,29 @@
+Palindrome Checker
+Write a method called palindrome? that takes a string as an argument and returns true if the string is a palindrome and false otherwise. A palindrome is a word, phrase, number, or other sequences of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
+
+Prime Numbers
+Create a method called prime? that takes an integer as an argument and returns true if the given number is a prime number and false otherwise. A prime number is a number greater than 1 that cannot be formed by multiplying two smaller natural numbers.
+
+Fibonacci Sequence
+Write a method called fibonacci that takes an integer n as an argument and returns the nth value in the Fibonacci sequence. The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones, starting from 0 and 1.
+
+Caesar Cipher
+Implement a method called caesar_cipher that takes a string and a number shift as arguments and returns the Caesar cipher of the given string with the given shift value. The Caesar cipher is a simple encryption technique that shifts each letter in the plaintext by a fixed number of positions down the alphabet.
+
+Longest Substring
+Write a method called longest_substring that takes a string as an argument and returns the longest substring without repeating characters.
+
+String Compression
+Implement a method called compress that takes a string as an argument and returns a new string with a basic compression applied. The compression should replace consecutive repeated characters with the character followed by the count of repetitions. If the compressed string would not become smaller than the original string, the method should return the original string.
+
+Integer to Roman
+Write a method called integer_to_roman that takes an integer as an argument and returns the Roman numeral representation of the given integer. The integer will be between 1 and 3999.
+
+Missing Number
+Create a method called missing_number that takes an array of integers as an argument and returns the missing number in the given range. The array will have no duplicates and contain integers from 1 to n, with one missing number.
+
+Two Sum
+Write a method called two_sum that takes an array of integers and a target integer as arguments and returns an array of indices of the two numbers in the input array such that they add up to the target. Assume that each input has exactly one solution, and you may not use the same element twice.
+
+Word Frequency
+Implement a method called word_frequency that takes a string as an argument and returns a hash with each unique word in the string as keys and the number of times they appear in the string as values. Ignore capitalization and punctuation.
